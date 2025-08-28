087fad1032699dc733e6f1bf362e82fb7f12a22d
JessicaGrisales <pi43jro@eduvaud.ch>
2025-08-28 11:01:25 +0200

    feat(C#)exercice1 terminé

---
+++ JessicaGB/exo1/Exercice_Marché/Exercice_Marché/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Marché
+{
+    internal class Product
+    {
+        public string Location { get; set; }
+        public string Provider { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public decimal Price { get; set; }
+
+
+
+       
+    }
+}
---
+++ JessicaGB/exo1/Exercice_Marché/Exercice_Marché/Produit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Marché
+{
+    internal class Produit
+    {
+        public int Location { get; set; }
+        public string Producteur { get; set; }
+        public string ProduitNom { get; set; }
+        public int Quantité { get; set; }
+        public int Unité { get; set; }
+        public double PrixUnité { get; set; }
+
+    }
+}
